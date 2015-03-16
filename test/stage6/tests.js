4a068ff3135fddbd7ff1e15a7319068335806e22
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-03-16 22:40:51 +0900

    Implement stage6

---
+++ test/stage6/tests.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('ステージ6（意図通りにモジュールを書ける）', function() {
+  it('あなたの満足のいくアプリケーションがここにある', function() {
+    var qualityOfYourAppliation = undefined;
+
+    expect(qualityOfYourAppliation).to.be.ok;
+  });
+});
