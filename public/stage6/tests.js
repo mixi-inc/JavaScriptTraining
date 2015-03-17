9124b06a822a305f16dd6150c6f20981c23e39c6
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-03-17 18:52:06 +0900

    Improve stage6

--- public/stage6/tests.js
+++ public/stage6/tests.js
@@ -1,7 +1,10 @@
 'use strict';
 
 describe('ステージ6（意図通りにモジュールを書ける）', function() {
-  it('あなたの満足のいくアプリケーションがここにある', function() {
+  it('あなたの満足のいく Web アプリケーションがここにある', function() {
+    // ボタンはサービスです。
+    // 自由に使ってください。
+
     var qualityOfYourAppliation = undefined;
 
     expect(qualityOfYourAppliation).to.be.ok;
